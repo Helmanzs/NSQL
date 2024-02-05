b7997ab520d514c4b786703e55d7e4f23dd55707
Helmanz <helcze4@gmail.com>
2024-02-05 23:09:48 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'Hello, Flask with MongoDB!'
 
-@app.route("/mongo/")
-def hello_mongo():
-    return "<p>Hello, Mongo!</p>"
+uri = "mongodb+srv://nsql:nsql@cluster0.hopgxl2.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client["sample_analytics"]
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+@app.route('/users')
+def get_users():
+    customers = db.customers
+    users = list(customers.find())
+    for user in users:
+        if 'tier_and_details' in user:
+            user['tier_and_details'] = str(user['tier_and_details'])
+
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
